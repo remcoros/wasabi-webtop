a224e55860a189daccdcd68fbf2d4b388de1575e
Remco Ros <remcoros@live.nl>
2025-08-13 12:40:42 +0000

    fix TITLE env var

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -14,7 +14,7 @@ chown -R 1000:1000 /config/.config/openbox
 
 # set default TITLE if it's not defined
 if [ -z "$TITLE" ]; then
-  TITLE="Wasabi Wallet"
+  export TITLE="Wasabi Wallet"
 fi
 
 # set RECONNECT to 1 if it's not defined
